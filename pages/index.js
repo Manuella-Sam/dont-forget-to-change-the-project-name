8c7a609262e0d9116f4b86069e73144f189a7fc8
Manuella Bagoussa <samanthabagoussa@gmail.com>
2023-08-07 10:36:54 +0200

    package-lock.json

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { JobsDummyData } from "@/public/Job.Data";
 export default function HomePage() {
+  const result = JobsDummyData.filter((item) => item.category === "back-end");
+console.log(result);
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>{result}</h1>
     </div>
   );
 }
+  
---
+++ public/Job.Data.js
@@ -0,0 +1,113 @@
+
+export const JobsDummyData =
+    [{
+        Jobtitle: "UI Designer",
+        category  : "UX",
+        salary: 25,
+        company: "marcos",
+        rating: 4
+      },
+      {
+        Jobtitle: " Front-end Developer",
+        category  : "Front-end",
+        salary: 109,
+        company: "liddy",
+        rating: 5
+      },
+      {
+        Jobtitle: "Web Developer",
+        category  :"Front-end",
+        salary: 309,
+        company: "liddy",
+        rating: 4.9
+      },
+      {
+        Jobtitle: "Online Marketing",
+        category  :"Online Marketing",
+        salary: 125,
+        company: "marcos",
+        rating: 4.8
+      },
+      {
+        Jobtitle: "Back-end Developer",
+        category :"back-end",
+        salary: 40,
+        company: "liddy",
+        rating: 4.6
+      },
+      {
+        Jobtitle: "Software Engineer",
+        category : "Full-stack",
+        salary: 42,
+        company: "ikea",
+        rating: 4.55
+      },
+      {
+        Jobtitle: "Graphic Designer",
+        category :"UX",
+        salary: 23,
+        company: "ikea"
+      },
+      {
+        Jobtitle: " Web Developer",
+        category :"Front-end",
+        salary: 59,
+        featured: true,
+        company: "caressa"
+      },
+      {
+        Jobtitle: "Web Designer",
+        category :"Full-stack",
+        salary: 39,
+        featured: true,
+        company: "ikea"
+      },
+      
+      {
+        Jobtitle: "Back-end Developer",
+        category :"back-end",
+        salary: 99,
+        company: "caressa"
+      },
+      {
+        Jobtitle: "UX Designer",
+        category :"UX",
+        salary: 31,
+        company: "caressa"
+      },
+      {
+        Jobtitle: "Social Media Manager",
+        category :"Online Marketing",
+        salary: 30,
+        company: "liddy"
+      },
+      {
+        Jobtitle: "App Designer",
+        category :" UX",
+        salary: 30,
+        company: "ikea"
+      },
+      {
+        Jobtitle: "UX Designer",
+        category  : "UX",
+        salary: 109,
+        company: "liddy"
+      },
+      {
+        Jobtitle: "Web Developer",
+        category  : "Full-stack",
+        salary: 129,
+        company: "marcos"
+      },
+      {
+        Jobtitle: "Software Developer",
+        category  : "Full-stack",
+        salary: 15,
+        company: "caressa"
+      },
+      ]
+      
+      
+      
+          
+  
