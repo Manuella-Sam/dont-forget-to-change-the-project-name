0a4156d98e33aa11ea4f0911315c78c7cf3d46a0
Manuella Bagoussa <samanthabagoussa@gmail.com>
2023-08-07 12:46:39 +0200

    pages/components/         postcss.config.js         tailwind.config.js

---
+++ pages/components/MyListContainer.js
@@ -0,0 +1,8 @@
+import React from `react`
+
+export const MyListContainer = () => {
+    return (
+        <div>MyListContainer</div>
+
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import { JobsDummyData } from "@/public/Job.Data";
-export default function HomePage() {
-  const result = JobsDummyData.filter((item) => item.category === "back-end");
-console.log(result);
+import { MyListContainer } from "./components/MyListContainer";
+// export default function HomePage() {
+//   const result = JobsDummyData.filter((item) => item.category === "back-end");
+// console.log(result);
+//   return (
+//     <div>
+//       <h1>{result}</h1>
+//     </div>
+//   );
+// }
+  
+// var newArray = homes.filter(function (el) {
+//   return el.price <= 1000 &&
+//          el.sqft >= 500 &&
+//          el.num_of_beds >=2 &&
+//          el.num_of_baths >= 2.5;
+// });
+
+export default function Homepage() {
   return (
     <div>
-      <h1>{result}</h1>
+      <MyListContainer/>
+
     </div>
-  );
-}
-  
+  )
+  }
