efc0fc789c97bc6484c10bdf7f999ae36031c075
Manuella Bagoussa <samanthabagoussa@gmail.com>
2023-08-10 14:19:24 +0200

    FlexBox

--- pages/job-category/index.js
+++ pages/job-category/index.js
@@ -4,6 +4,7 @@ import JobList from "../components/JobList";
 import { v4 as uuidv4 } from "uuid";
 import tailwindConfig from "@/tailwind.config";
 import postcssConfig from "@/postcss.config";
+import Image from "next/image";
 
 const StyledArticle = styled.article`
   border-radius: 20px;
@@ -17,50 +18,140 @@ const StyledArticle = styled.article`
   font-size: 20px;
 `;
 
-const StyledHeading = styled.h1``;
+const StyledHeading = styled.h1`
+  text-align: center;
+  color: white;
+`;
 const StyledSpan = styled.span`
   border-radius: 20px;
-  border: 1px solid lightgrey;
+  border: 1px lightgrey;
+  box-shadow: 1px 1px 2px lightgrey;
+  background-color: #c8c8c8;
   margin: 5px;
   padding: 10px;
-  width: 70%;
+  width: 400px;
   display: flex;
   flex-direction: column;
+
   align-items: center;
   font-size: 20px;
 `;
-const StyledSection = styled.section``;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10%;
+  webkit-box-pack: center;
+  flex-direction: row;
+  justify-content: center;
+  background-color: white;
+`;
+
+const StyledDiv1 = styled.div`
+  display: flex;
+  text-align: center;
+  flex-align: center;
+  postion: absolute;
+  left: 30px;
+`;
+
+const StyledP = styled.p`
+  color: white;
+`;
+
+const StyledSection = styled.section`
+  postion: absolute;
+  width: 80 %;
+`;
+
+const StyledSpan1 = styled.span``;
 
 export default function createJobcategory() {
   return (
-    <div class="mt 8 grid grid-cols-5">
-      <div class="card">
+    <StyledDiv>
+      <StyledDiv1>
         <StyledSpan>
+          <Image
+            src="/1.jpg"
+            width={400}
+            height={200}
+            alt="Picture of the author"
+          />
           <StyledHeading>Full-Stack</StyledHeading>
+          <StyledP>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            iaculis, nulla ac convallis rutrum, erat enim porta dui, id lobortis
+            ex nulla vel leo.
+          </StyledP>
         </StyledSpan>
-      </div>
+      </StyledDiv1>
+
       <div class="card">
         <StyledSpan>
+          <Image
+            src="/7.jpg"
+            width={400}
+            height={200}
+            alt="Picture of the author"
+          />
           <StyledHeading>Front-End</StyledHeading>
+          <StyledP>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            iaculis, nulla ac convallis rutrum, erat enim porta dui, id lobortis
+            ex nulla vel leo.
+          </StyledP>
         </StyledSpan>
       </div>
       <div class="card">
         <StyledSpan>
+          <Image
+            src="/5.jpg"
+            width={400}
+            height={200}
+            alt="Picture of the author"
+          />
           <StyledHeading>UX</StyledHeading>
+          <StyledP>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            iaculis, nulla ac convallis rutrum, erat enim porta dui, id lobortis
+            ex nulla vel leo.
+          </StyledP>
         </StyledSpan>
       </div>
 
       <div class="card">
         <StyledSpan>
+          <Image
+            src="/4.jpg"
+            width={400}
+            height={200}
+            alt="Picture of the author"
+          />
           <StyledHeading>Online-Marketing</StyledHeading>
+          <StyledP>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            iaculis, nulla ac convallis rutrum, erat enim porta dui, id lobortis
+            ex nulla vel leo.
+          </StyledP>
         </StyledSpan>
       </div>
 
-      <div class="card">
+      <StyledSection>
         <StyledSpan>
+          <Image
+            src="/3.jpg"
+            width={400}
+            height={200}
+            alt="Picture of the author"
+          />
           <StyledHeading>Back-end</StyledHeading>
+          <StyledP>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            iaculis, nulla ac convallis rutrum, erat enim porta dui, id lobortis
+            ex nulla vel leo.
+          </StyledP>
         </StyledSpan>
-      </div>
-    </div>
+      </StyledSection>
+    </StyledDiv>
   );
 }
