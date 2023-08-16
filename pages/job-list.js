40cbda89292dee817b9e2cc79dea0211bc59ce53
Manuella Bagoussa <samanthabagoussa@gmail.com>
2023-08-16 11:42:03 +0200

    command Z

--- pages/job-list.js
+++ pages/job-list.js
@@ -1,57 +1,20 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
-import { fetcher } from "@/utils/fetcher";
-import { v4 as uuidv4 } from "uuid";
 
-const StyledSection = styled.section`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`;
-const StyledHeading = styled.h1`
-  display: flex;
-  text-align: center;
-  font-size: 40px;
-  color: darkblue;
-`;
-const StyledArticle = styled.article`
-  border-radius: 20px;
-  border: 1px solid lightgrey;
-  margin: 5px;
-  padding: 10px;
-  width: 70%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: 20px;
-`;
-
-const StyledSpan = styled.span`
-  font-size: 27px;
-  color: darkblue;
-`;
-
-export default function joblist({ data }) {
-  const router = UseRouter();
-  const { slug: websiteSlug } = router.query;
-  data = UseSWR(
-    `https://arbeitnow.com/api/job-board-api${websiteSlug}`,
-    fetcher
-  );
-
+export default function joblist({ jobs }) {
+  const router = useRouter;
   return (
     <>
       <StyledSection>
         <StyledHeading>Job List</StyledHeading>
-        {datas.map((data) => {
+        {jobs.map((job) => {
           return (
             <StyledArticle key={uuidv4()}>
               <StyledSpan>
-                <p>{data.title}</p>
+                <p>{job.title}</p>
               </StyledSpan>
 
-              {data.location}
+              {job.location}
             </StyledArticle>
           );
         })}
