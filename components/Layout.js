7a6fc0f0cd14a1edf855c71860072b4b08630ab8
Manuella Bagoussa <samanthabagoussa@gmail.com>
2023-08-17 16:41:38 +0200

    trying to pass down prop

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import styled from "styled-components";
 import Link from "next/link";
 
@@ -7,13 +8,14 @@ const Navigation = styled.nav`
   left: 0;
   width: 100%;
   display: flex;
-  background-color: teal;
+
+  gap: 1 em;
+  background-color: lightblue;
   color: white;
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  gap: 1.5em;
 `;
 
 const Header = styled.header`
@@ -22,23 +24,86 @@ const Header = styled.header`
   left: 0;
   width: 100%;
   display: flex;
-  background-color: teal;
+  justify-content: center;
+  background-color: lightblue;
   color: white;
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  color: white;
+  display: flex;
+
+  gap: 1.5em;
 `;
 
 const NavigationList = styled.ul``;
 
 const NavigationListItem = styled.li``;
 
-export default function Layout({ children }) {
+// const jobSeekingData = [
+//   {
+//     jobtitle: "UX Designer with Development Background",
+//     start: "December 2023",
+//     salary: "60,000",
+//     desc: "I am looking for a job that will meet my expectations",
+//     tags: ["Designer", "Developer"],
+//   },
+
+const StyledArticle = styled.article`
+  border-radius: 20px;
+  border: 1px solid lightgrey;
+  margin: 5px;
+  padding: 10px;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 20px;
+`;
+
+const StyledHeading = styled.h1`
+  display: flex;
+  text-align: center;
+  font-size: 40px;
+  color: darkblue;
+`;
+
+const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledSpan = styled.span`
+  font-size: 27px;
+  color: darkblue;
+`;
+
+export default function Layout({ children, users }) {
   return (
     <>
-      <Header>Your App Name</Header>
+      <Header>My App Name</Header>
       <Wrapper>{children}</Wrapper>
+
+      <StyledSection>
+        <StyledHeading>Job List</StyledHeading>
+        {users.map((user) => {
+          return (
+            <StyledArticle key={uuidv4()}>
+              <StyledSpan>
+                <p>{users.jobtitle}</p>
+              </StyledSpan>
+              {users.start}
+              {users.salary}
+              {users.description}
+              <p>{users.tags}</p>
+            </StyledArticle>
+          );
+        })}
+      </StyledSection>
+
       <Navigation>
         <NavigationList>
           <NavigationListItem>
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
 import { useState } from "react";
 import GlobalStyle from "../styles";
 import { JobsDummyData } from "./api/job_offers";
+import { jobSeekingData } from "./api/job_seeking";
 
 import Layout from "@/components/Layout";
 
+// const jobSeekingData = [
+//   {
+//     jobtitle: "UX Designer with Development Background",
+//     start: "December 2023",
+//     salary: "60,000",
+//     desc: "I am looking for a job that will meet my expectations",
+//     tags: ["Designer", "Developer"],
+//   },
+
 export default function App({ Component, pageProps }) {
   const [jobs, setJobs] = useState(JobsDummyData);
+  const [users, setUsers] = useState(jobSeekingData);
   return (
     <>
       <GlobalStyle />
       <Layout>
-        <Component {...pageProps} jobs={jobs} />
+        <Component {...pageProps} jobs={jobs} users={users} />
       </Layout>
     </>
   );
