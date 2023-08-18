1b53b50e8a38c001db816de0de9bfbe8b55242b5
Manuella Bagoussa <samanthabagoussa@gmail.com>
2023-08-18 14:01:53 +0200

    switches jobseekers page content with job-seeking

--- pages/job-seeking/index.js
+++ pages/job-seeking/index.js
@@ -1,3 +1,55 @@
-export default function JobSeeking() {
-  return <div>Job Cards go here</div>;
+import styled from "styled-components";
+import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
+
+const StyledArticle = styled.article`
+  border-radius: 20px;
+  border: 1px solid lightgrey;
+  margin: 5px;
+  padding: 10px;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 20px;
+`;
+
+const StyledHeading = styled.h1`
+  display: flex;
+  text-align: center;
+  font-size: 40px;
+  color: darkblue;
+`;
+
+const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledSpan = styled.span`
+  font-size: 27px;
+  color: darkblue;
+`;
+
+export default function Page({ seekingUsers }) {
+  return (
+    <StyledSection>
+      <StyledHeading>Users</StyledHeading>
+      {seekingUsers.map((seekingUser) => {
+        return (
+          <StyledArticle key={uuidv4()}>
+            <StyledSpan>
+              <p>{seekingUser.jobtitle}</p>
+            </StyledSpan>
+            <p>{seekingUser.start}</p>
+            <p>{seekingUser.salary}</p>
+            <p>{seekingUser.desc}</p>
+            <p>{seekingUser.tags}</p>
+          </StyledArticle>
+        );
+      })}
+    </StyledSection>
+  );
 }
